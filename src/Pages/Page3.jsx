6b0e9d12e99858fc036e5e477fcad403e33a5f44
GregorHorvatH <gregor.horvath83@gmail.com>
2024-01-18 12:07:20 +0200

    add Intercom

--- src/Pages/Page3.jsx
+++ src/Pages/Page3.jsx
@@ -1,7 +1,23 @@
 import { Layout } from '../Components';
 
-export const Page3 = () => (
-  <Layout>
-    <p>Page 3</p>
-  </Layout>
-);
+export const Page3 = () => {
+  const handleBoot = () => {
+    if (typeof window.Intercom === 'function') {
+      window.Intercom('boot', window.intercomSettings);
+    }
+  };
+
+  const handleShutDown = () => {
+    if (typeof window.Intercom === 'function') {
+      window.Intercom('shutdown');
+    }
+  };
+
+  return (
+    <Layout>
+      <p>Page 3</p>
+      <button onClick={handleBoot}>Intercom Boot</button>
+      <button onClick={handleShutDown}>Intercom Shut Down</button>
+    </Layout>
+  );
+};
